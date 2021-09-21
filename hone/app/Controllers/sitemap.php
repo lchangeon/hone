459750869ed60652a2e02a89742e586637b7653e
Ludovic Changeon <lchangeon@gmail.com>
2021-09-21 15:14:00 +0900

    Add siteMap template controller

---
+++ hone/app/Controllers/sitemap.php
@@ -0,0 +1,42 @@
+<?php 
+
+/*
+ * Copyright 2021 Ludovic CHANGEON 
+ * License : LGPL-3.0-or-later
+ * 
+ * This file is part of Hone Framework.
+ *
+ * Hone Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Hone Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Hone Framework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+
+class Sitemap extends Controller{
+
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function index() {
+        $this->displayMap();
+    }
+
+    public function displayMap(){
+        echo 'Add here siteMap content you want to return to browser.';
+    }
+    
+}
+
